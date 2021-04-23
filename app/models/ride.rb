72bf2d06fde0cc14ed2672a1ab247b1a78b70ed6
Aiden <aiden.marques94@gmail.com>
2021-04-22 21:59:23 -0400

    commit tree b015050a1bfbcec7ad20d6fcf19b940958d10369

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,12 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user 
+    belongs_to :attraction
+    
+    # validates :user_id, presence: true
+    # validates :attraction_id, presence: true
+
+    def take_ride
+
+    end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+    # validates :name, presence: true
+    # validates :happiness, presence: true
+    # validates :nausea, presence: true
+    # validates :password, presence: true
+    # validates :height, presence: true
+    # validates :tickets, presence: true
 end
